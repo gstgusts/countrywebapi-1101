93b80008732711deaf38fbbfbca15baacd85c721
Gusts Linkevics <gusts@gst.lv>
2021-04-12 21:00:13 +0300

    POST new Country endpoint

--- src/main/java/com/example/countrywebapi/controllers/CountryController.java
+++ src/main/java/com/example/countrywebapi/controllers/CountryController.java
@@ -3,9 +3,7 @@ package com.example.countrywebapi.controllers;
 import com.example.countrywebapi.data.Country;
 import com.example.countrywebapi.data.CountryRepository;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -20,5 +18,26 @@ public class CountryController {
    @GetMapping("/countries")
    public Iterable<Country> getCountries() {
       return repo.getCountries();
+   }
+
+   @GetMapping("/countries/{id}")
+   public Country getCountry(@PathVariable int id) {
+      return repo.getById(id);
+   }
+
+   @GetMapping("/countries/search")
+   public Iterable<Country> getCountry(@RequestParam String name) {
+      return repo.searchByName(name);
+   }
+
+   @PostMapping("/countries")
+   public Country addCountry(@RequestBody Country country)
+   {
+      if(country == null) {
+         return null;
+      }
+
+       repo.save(country);
+       return country;
    }
 }
--- src/main/java/com/example/countrywebapi/data/CountryRepository.java
+++ src/main/java/com/example/countrywebapi/data/CountryRepository.java
@@ -2,7 +2,9 @@ package com.example.countrywebapi.data;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,5 +36,54 @@ public class CountryRepository {
         }
 
         return new ArrayList<>();
+    }
+
+    public Country getById(int id) {
+        var session = factory.openSession();
+
+        try {
+            return session.get(Country.class, id);
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+
+        return null;
+    }
+
+    public List<Country> searchByName(String name) {
+        var session = factory.openSession();
+
+        try {
+            String hql = "FROM Country C WHERE C.name like :name";
+            Query query = session.createQuery(hql);
+            query.setParameter("name","%"+name+"%");
+            return query.list();
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+
+        return new ArrayList<>();
+    }
+
+    public void save(Object item) {
+        var session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.save(item);
+            tx.commit();
+        } catch (HibernateException ex) {
+            if(tx != null) {
+                tx.rollback();
+            }
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
     }
 }
